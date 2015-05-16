1b8427d314c7213ba742a5a8c11292f6f9e01abc
Ben Morton <ben.morton91@gmail.com>
2015-05-17 00:34:32 +0100

    Change the name, remove the apostrophe

--- Alzheimer's Clock/App.xaml.cs
+++ Alzheimer's Clock/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Alzheimer_s_Clock
+namespace Alzheimers_Clock
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Alzheimer's Clock/MainWindow.xaml.cs
+++ Alzheimer's Clock/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Alzheimer_s_Clock
+namespace Alzheimers_Clock
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
